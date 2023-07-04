dd00915cf855336f852d7a90b8717c572d0cee78
rurze <rurze1@gmail.com>
2023-07-04 16:07:36 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectado ao bando de dados")
+console.log("O paulo fez esse commit sem voce ver a aula")
+console.log("Enviando para o GitHub")
