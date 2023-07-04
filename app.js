1870c758aff5876fa5e1c1fd746ae0c4400aeaf9
rurze <rurze1@gmail.com>
2023-07-04 16:29:06 -0300

    voltando para  atras

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectado ao bando de dados")
-console.log("O paulo fez esse commit sem voce ver a aula")
-console.log("Enviando para o GitHub")
